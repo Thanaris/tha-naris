ceea616b896d13bcb6720337a1c71e80c5ed0f38
Thanaris <afxx91@gmail.com>
2025-08-20 12:35:06 +0200

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from 'next/image';
-import Link from 'next-intl/link';
+import {Link} from 'next-intl/navigation';
 import {useTranslations} from 'next-intl';
 
 export default function Header() {
